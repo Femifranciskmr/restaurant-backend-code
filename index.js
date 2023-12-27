eaa6911b594c88bc9c46ff4ba5e4ad798dc97317
Femifranciskmr <femifranciskmr@gmail.com>
2023-12-27 14:02:35 +0530

    restaurant_backend

---
+++ index.js
@@ -0,0 +1,29 @@
+//import jison-server
+const jsonServer=require('json-server')
+
+//create a srever application using jsonServer
+const restServer=jsonServer.create()
+
+
+//setup path for db.json
+const router=jsonServer.router()
+
+
+//return a middleware used by json server
+const middleware=jsonServer.defaults()
+
+
+//setup port
+const port=3001
+
+
+//use in server
+restServer.us(middleware)
+restServer.use(router)
+
+
+
+//to run a port
+restServer.listen(port,()=>{
+    console.log('rest server listening on port' +port);
+})
